620d2286dcf86afb60270dbc632f0585f5ec0ea8
Josh Rose <josh@etch.co>
2022-12-19 15:26:28 +0100

    feat: initial stylelint setup

---
+++ README.md
@@ -0,0 +1,15 @@
+# @etchteam/eslint
+
+The eslint config that we use at [Etch](https://etch.co)
+
+## Install
+
+```bash
+npm install eslint @etchteam/eslint
+```
+
+## Use
+
+```bash
+echo "module.exports = { extends: ['@etchteam/eslint'] }" > .eslintrc.js
+```
---
+++ src/index.js
@@ -0,0 +1,32 @@
+module.exports = {
+  extends: [
+    'stylelint-config-sass-guidelines',
+    'stylelint-prettier/recommended',
+  ],
+  plugins: [
+    'stylelint-order',
+    'stylelint-scss',
+    'stylelint-selector-bem-pattern',
+  ],
+  rules: {
+    'selector-class-pattern': null,
+    'plugin/selector-bem-pattern': {
+      preset: 'bem',
+      componentName: '[A-Z]+',
+      componentSelectors: {
+        initial: '^\\.{componentName}(?:-[a-z]+)?$',
+        combined: '^\\.combined-{componentName}-[a-z]+$',
+      },
+      utilitySelectors: '^\\.util-[a-z]+$',
+    },
+    'max-nesting-depth': 4,
+  },
+  ignoreFiles: [
+    "node_modules",
+    "bin",
+    "obj",
+    "*.*",
+    "!*.css",
+    "!*.scss"
+  ]
+};
